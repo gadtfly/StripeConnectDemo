5c3169a8e56d062f86e8c03287c3492de596e62b
Adam Louis <adamlouis@gmail.com>
2014-08-05 20:18:41 -0700

    add stripe server-side (charges actually executed)

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -4,7 +4,18 @@ class TipsController < ApplicationController
   end
 
   def create
-    current_user.tips_given.create(tip_params)
+    tip = current_user.tips_given.new(tip_params)
+    Stripe::Charge.create({
+      card: params[:stripeToken],
+      amount: tip.amount_in_cents,
+      description: tip.description,
+      currency: 'usd'},
+      tip.tippee.stripe_secret_key
+    )
+    tip.save
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+  ensure
     redirect_to :back
   end
 
--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,4 +1,12 @@
 class Tip < ActiveRecord::Base
   belongs_to :tipper, class_name: :User
   belongs_to :tippee, class_name: :User
+
+  def amount_in_cents
+    (amount * 100).to_i
+  end
+
+  def description
+    "#{tipper.email} tipping #{tippee.email}"
+  end
 end
